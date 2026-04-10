941992dabeebd7b02031e1c9bca38139dbbb88e0
suchitrachowdhury219-ui <suchitrachowdhury219@gmail.com>
2026-04-10 23:27:52 +0530

    ..

--- 09_advanced_one/promises.js
+++ 09_advanced_one/promises.js
@@ -1,3 +1,5 @@
 //Promise object represents the eventual completion(or failure) of an asymchronous operation and its resulting value
     //3 state pending, fulfil, rejected
    const promise1=
+   //taking a break
+   
