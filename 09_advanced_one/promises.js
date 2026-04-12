9aed134054d7ee38058732b2e4b8575d86421fa7
suchitrachowdhury219-ui <suchitrachowdhury219@gmail.com>
2026-04-12 23:27:51 +0530

    promises

--- 09_advanced_one/promises.js
+++ 09_advanced_one/promises.js
@@ -1,5 +1,26 @@
-//Promise object represents the eventual completion(or failure) of an asymchronous operation and its resulting value
+//Promise is a object that represents the eventual completion(or failure) of an asymchronous operation and its resulting value
     //3 state pending, fulfil, rejected
-   const promise1=
-   //taking a break
+    //
+   const promise1=new Promise(function (resove,reject){
+    //do an async task
+    //DB calls, cryptography,Network call
+    setTimeout(function(){
+        console.log('Async task is completed');
+        //method to connect resolve with .then() and reject with .catch()
+        resolve();
+        
+    },1000)
+   });
+   //promise will be consumed, and how consumtion will be done we will see here
+   //resolve is connevten with .then()
+   promise1.then(function(){
+    console.log("Promise consumed");
+    
+   });
+
+   new Promise(function(resolve,reject){
+    
+   })
+
+   
    
