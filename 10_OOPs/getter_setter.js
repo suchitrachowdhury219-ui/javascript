0afc53342a67064cc83f25dce303f231f11aac49
suchitrachowdhury219-ui <suchitrachowdhury219@gmail.com>
2026-05-01 23:35:54 +0530

    basics on getters and setters

---
+++ 10_OOPs/getter_setter.js
@@ -0,0 +1,9 @@
+class User{
+    constructor(email,password){
+        this.email=email;
+        this.password=password;
+    }
+}
+const hitesh= new User("h@hitesh.ai","123")
+console.log();
+//getters and setters  are 
